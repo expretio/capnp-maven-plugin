a924caf356e8c2ceefbd006ed0632e9a4e26fceb
mtetreau <mtetreau@g75vm.expretio.com>
2015-12-07 17:28:11 -0500

    APPIAPLATFORM-5100: Updating tests.

--- src/test/java/com/expretio/maven/plugin/capnp/CapnpCompilerTest.java
+++ src/test/java/com/expretio/maven/plugin/capnp/CapnpCompilerTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import com.expretio.maven.plugins.capnp.CapnpCompiler;
@@ -33,8 +34,99 @@ public class CapnpCompilerTest
         compiler.compile();
 
         // Verifying outcome
+        assertThat(workDirectory)
+            .contains("com/expretio/maven/plugins/capnp/alpha/alpha.capnp")
+            .contains("com/expretio/maven/plugins/capnp/beta/beta.capnp")
+            .contains("capnp")
+            .contains("capnpc-java")
+            .contains("java.capnp");
 
+        assertThat(outputDirectory)
+            .contains("com/expretio/maven/plugins/capnp/alpha/AlphaCapnp.java")
+            .contains("com/expretio/maven/plugins/capnp/beta/BetaCapnp.java");
     }
 
+    @Test(expected = MojoFailureException.class)
+    public void withoutOutputDirectory() throws MojoFailureException, MojoExecutionException
+    {
+        // Setting up fixture
+        CapnpCompiler compiler = CapnpCompiler.builder()
+            .setSchemaBaseDirectory(schemaBaseDirectory)
+            .setWorkDirectory(workDirectory)
+            .addSchema(alphaSchema)
+            .addSchema(betaSchema)
+            .build();
 
+        // Exercising system under test
+        compiler.compile();
+    }
+
+    @Test(expected = MojoFailureException.class)
+    public void withoutSchemaBaseDirectory() throws MojoFailureException, MojoExecutionException
+    {
+        // Setting up fixture
+        CapnpCompiler compiler = CapnpCompiler.builder()
+            .setOutputDirectory(outputDirectory)
+            .setWorkDirectory(workDirectory)
+            .addSchema(alphaSchema)
+            .addSchema(betaSchema)
+            .build();
+
+        // Exercising system under test
+        compiler.compile();
+    }
+
+    @Test(expected = MojoFailureException.class)
+    public void withoutWorkingDirectory() throws MojoFailureException, MojoExecutionException
+    {
+        // Setting up fixture
+        CapnpCompiler compiler = CapnpCompiler.builder()
+            .setOutputDirectory(outputDirectory)
+            .setSchemaBaseDirectory(schemaBaseDirectory)
+            .addSchema(alphaSchema)
+            .addSchema(betaSchema)
+            .build();
+
+        // Exercising system under test
+        compiler.compile();
+    }
+
+    @Test(expected = MojoFailureException.class)
+    public void withoutSchema() throws MojoFailureException, MojoExecutionException
+    {
+        // Setting up fixture
+        CapnpCompiler compiler = CapnpCompiler.builder()
+            .setOutputDirectory(outputDirectory)
+            .setSchemaBaseDirectory(schemaBaseDirectory)
+            .setWorkDirectory(workDirectory)
+            .build();
+
+        // Exercising system under test
+        compiler.compile();
+    }
+
+    // [Utilities]
+
+    private static DirectoryAssert assertThat(File directory)
+    {
+        return new DirectoryAssert(directory);
+    }
+
+    private static class DirectoryAssert
+    {
+        private File directory;
+
+        public DirectoryAssert(File directory)
+        {
+            this.directory = directory;
+        }
+
+        public DirectoryAssert contains(String filename)
+        {
+            Assertions.assertThat(
+                new File(directory, filename)).exists();
+
+            return this;
+        }
+    }
 }
