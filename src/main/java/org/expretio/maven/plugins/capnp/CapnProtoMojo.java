aa619dec3b854e72654c81644f62219af9aafe06
mtetreau <marc.tetreau@expretio.com>
2016-01-12 19:54:39 -0500

    removing monitoring

--- src/main/java/org/expretio/maven/plugins/capnp/CapnProtoMojo.java
+++ src/main/java/org/expretio/maven/plugins/capnp/CapnProtoMojo.java
@@ -248,9 +248,6 @@ public class CapnProtoMojo
             }
         }
 
-        // FIXME: Remove
-        System.out.println("bidon allschemas: " + allSchemas);
-
         return allSchemas;
     }
 
